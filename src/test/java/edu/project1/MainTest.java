51a932d54dc40070a501b89eac66ab7a4b6e9886
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-10-22 18:26:26 +0700

    + mainTest

--- src/test/java/edu/project1/MainTest.java
+++ src/test/java/edu/project1/MainTest.java
@@ -1,4 +1,22 @@
 package edu.project1;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class MainTest {
+
+    @Test
+    @DisplayName("test")
+    public void test1() {
+        String input = "Hello\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        String ans = "Hello";
+        String result = Main.nextInput();
+        assertThat(ans).isEqualTo(result);
+    }
 }
