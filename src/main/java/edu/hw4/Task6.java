9ab11de47f2a5e94894a090cbb483d659f8e0273
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-01 21:03:02 +0700

    Create Task6.java

---
+++ src/main/java/edu/hw4/Task6.java
@@ -0,0 +1,23 @@
+package edu.hw4;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Task6 {
+
+    public static Map<Animal.Type, Animal> heaviestAnimalEveryType (List<Animal> animals) {
+        HashMap<Animal.Type, Animal> tableOfSeverity = new HashMap<>();
+
+        tableOfSeverity.put(Animal.Type.CAT, null);
+        tableOfSeverity.put(Animal.Type.DOG, null);
+        tableOfSeverity.put(Animal.Type.BIRD, null);
+        tableOfSeverity.put(Animal.Type.FISH, null);
+        tableOfSeverity.put(Animal.Type.SPIDER, null);
+
+        for (Animal animal : animals) {
+            
+        }
+
+    }
+}
