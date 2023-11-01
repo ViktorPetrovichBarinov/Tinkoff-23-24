2c12563d192a6a58102950bd9fc5c56d461e533e
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-01 20:58:42 +0700

    5

--- src/main/java/edu/hw4/Task5.java
+++ src/main/java/edu/hw4/Task5.java
@@ -1,4 +1,24 @@
 package edu.hw4;
 
+import java.util.List;
+
 public class Task5 {
+
+    public static Animal.Sex mostPopularGender(List<Animal> animals) {
+        int MaleCounter = 0;
+        int FemaleCounter = 0;
+        for(Animal animal : animals) {
+            if (animal.sex() == Animal.Sex.M) {
+                MaleCounter++;
+            } else {
+                FemaleCounter++;
+            }
+        }
+        if(MaleCounter >= FemaleCounter) {
+            return Animal.Sex.M;
+        } else {
+            return Animal.Sex.F;
+        }
+    }
+
 }
