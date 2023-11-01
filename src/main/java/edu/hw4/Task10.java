28cc61c233c88bc799fe5ba7b512b79dcc7d877f
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-01 22:09:26 +0700

    10

---
+++ src/main/java/edu/hw4/Task10.java
@@ -0,0 +1,16 @@
+package edu.hw4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task10 {
+    public static List<Animal> listOfAnimalsPawsNotEqualAges (List<Animal> animals) {
+        ArrayList<Animal> returnList = new ArrayList<>();
+        for(Animal animal : animals) {
+            if(animal.paws() != animal.age()) {
+                returnList.add(animal);
+            }
+        }
+        return returnList;
+    }
+}
