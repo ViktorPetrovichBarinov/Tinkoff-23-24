9aade26dfe0606b77512c7640617390b380ee3ba
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-03 01:58:41 +0700

    16

---
+++ src/main/java/edu/hw4/Task16.java
@@ -0,0 +1,19 @@
+package edu.hw4;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Task16 {
+
+    public static List<Animal> task16 (List<Animal> animals) {
+
+        animals.sort(Comparator
+            .comparing(Animal::type)
+            .thenComparing(Animal::sex)
+            .thenComparing(Animal::name)
+        );
+
+        return animals;
+    }
+}
