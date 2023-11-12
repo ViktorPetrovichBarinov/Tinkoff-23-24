9437d52b780ad0c89517e63b7dc00111b5202464
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-12 22:16:01 +0700

    task2 checkstyle

--- src/main/java/edu/hw5/Task2.java
+++ src/main/java/edu/hw5/Task2.java
@@ -3,18 +3,22 @@ package edu.hw5;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
-
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Task2 {
 
+    private Task2() {
+
+    }
+
+    private static final int THE_THIRTEENTH = 13;
 
     public static List<LocalDate> findFridaysThe13th(int year) {
         List<LocalDate> fridaysThe13th = new ArrayList<>();
         for (Month month : Month.values()) {
-            LocalDate date = LocalDate.of(year, month, 13);
+            LocalDate date = LocalDate.of(year, month, THE_THIRTEENTH);
 
             if (date.getDayOfWeek() == DayOfWeek.FRIDAY) {
                 fridaysThe13th.add(date);
@@ -26,7 +30,7 @@ public class Task2 {
     public static LocalDate findNextFridayThe13(LocalDate date) {
         LocalDate currentDate = date;
         currentDate = currentDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY));
-        while(currentDate.getDayOfMonth() != 13) {
+        while (currentDate.getDayOfMonth() != THE_THIRTEENTH) {
             currentDate = currentDate.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
         }
         return currentDate;
