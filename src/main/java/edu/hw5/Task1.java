a9cb10186e7b11d98eab9d354719629d0dbb003a
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-12 21:37:09 +0700

    task1 + test

--- src/main/java/edu/hw5/Task1.java
+++ src/main/java/edu/hw5/Task1.java
@@ -1,21 +1,51 @@
 package edu.hw5;
 
 import java.time.Duration;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Task1 {
-    public static void main(String[] args) {
-        LocalTime start = LocalTime.of(9, 0, 0);
-        LocalTime end = LocalTime.of(12, 30, 0);
+    private Task1() {
 
-        Duration duration = Duration.between(start, end);
-        System.out.println("Продолжительность: " + duration);
     }
+
+    private static final int NUMBER_OF_SECONDS_IN_MINUTES = 60;
+    private static final int NUMBER_OF_MINUTES_IN_HOUR = 60;
+    private static final int NUMBER_OF_HOURS_IN_DAY = 24;
 
     public static String calculateAverageTime(List<String> peopleSessions) {
-        
+        long totalTime = calculateTotalTime(peopleSessions);
+        long averageTime = totalTime / peopleSessions.size();
+
+        //выводим среднее значение сессии с точностью до минут в ввиде строки
+        long totalMinutes = averageTime / NUMBER_OF_SECONDS_IN_MINUTES;
+        long minutes = totalMinutes % NUMBER_OF_MINUTES_IN_HOUR;
+        long totalHours = totalMinutes / NUMBER_OF_MINUTES_IN_HOUR;
+        long hours = totalHours % NUMBER_OF_HOURS_IN_DAY;
+        long days = totalHours / NUMBER_OF_HOURS_IN_DAY;
+        StringBuilder answer = new StringBuilder();
+        answer.append(days).append(":")
+            .append(hours).append(":")
+            .append(minutes);
+
+        return answer.toString();
     }
 
+    private static long calculateTotalTime(List<String> peopleSessions) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd, HH:mm");
+        long totalTime = 0;
 
+        for (String session : peopleSessions) {
+            String[] dateTimes = session.split(" - ");
+            //получаем дату старта и окончания сеанса
+            LocalDateTime startDateTime = LocalDateTime.parse(dateTimes[0], formatter);
+            LocalDateTime endDateTime = LocalDateTime.parse(dateTimes[1], formatter);
+
+            //высчитываем разницу и представляем её в секундах
+            Duration duration = Duration.between(startDateTime, endDateTime);
+            totalTime += duration.toSeconds();
+        }
+        return totalTime;
+    }
 }
