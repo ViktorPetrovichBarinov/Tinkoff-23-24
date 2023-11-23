a2a1f30506da558eadd660e791573eb0d90ad8f5
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-24 04:14:18 +0700

    Test6
    добавил тест для 6 задачи

--- src/main/java/edu/hw6/Task6/PortScanner.java
+++ src/main/java/edu/hw6/Task6/PortScanner.java
@@ -3,63 +3,76 @@ package edu.hw6.Task6;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings({"MagicNumber", "RegexpSinglelineJava"})
 public class PortScanner {
-    private final static int LAST_PORT_NUMBER = 49151;
-    private static final Map<Integer, String> portMap = new HashMap<>();
+    private PortScanner() {
+    }
+
+    private static final Map<Integer, String> PORT_MAP = new HashMap<>();
+    private static final String EMPTY_DESCRIPTION = "---";
+    private static final String OUTPUT_FORMAT = "%-9s|%-6d|%s\n";
+
     static {
-        portMap.put(135, "EPMAP");
-        portMap.put(137, "Служба имен NetBIOS");
-        portMap.put(138, "Служба датаграмм NetBIOS");
-        portMap.put(139, "Служба сеансов NetBIOS");
-        portMap.put(445, "Microsoft-DS Active Directory");
-        portMap.put(843, "Adobe Flash");
-        portMap.put(1900, "Simple Service Discovery Protocol (SSDP)");
-        portMap.put(3702, "Динамическое обнаружение веб-служб");
-        portMap.put(5353, "Многоадресный DNS");
-        portMap.put(5355, "Link-Local Multicast Name Resolution (LLMNR)");
-        portMap.put(17500, "Dropbox");
-        portMap.put(27017, "MongoDB");
+        PORT_MAP.put(135, "EPMAP");
+        PORT_MAP.put(137, "Служба имен NetBIOS");
+        PORT_MAP.put(138, "Служба датаграмм NetBIOS");
+        PORT_MAP.put(139, "Служба сеансов NetBIOS");
+        PORT_MAP.put(445, "Microsoft-DS Active Directory");
+        PORT_MAP.put(843, "Adobe Flash");
+        PORT_MAP.put(1900, "Simple Service Discovery Protocol (SSDP)");
+        PORT_MAP.put(3702, "Динамическое обнаружение веб-служб");
+        PORT_MAP.put(5353, "Многоадресный DNS");
+        PORT_MAP.put(5355, "Link-Local Multicast Name Resolution (LLMNR)");
+        PORT_MAP.put(17500, "Dropbox");
+        PORT_MAP.put(27017, "MongoDB");
     }
 
 
     private static final String TCP = "TCP";
     private static final String UDP = "UDP";
 
-    public static void scanner() {
+    public static void scanner(List<Integer> ports) {
         System.out.println("Протокол  Порт   Сервис");
-        for (int port = 0; port <= 49151; port++) {
-            try {
-                ServerSocket serverSocket = new ServerSocket(port);
-                serverSocket.close();
-            } catch (IOException e) {
-                String service = portMap.get(port);
-                if(service == null) {
-                    service = "---";
+        for (Integer port : ports) {
+            if (isTcpBusy(port)) {
+                String service = PORT_MAP.get(port);
+                if (service == null) {
+                    service = EMPTY_DESCRIPTION;
                 }
-                System.out.printf("%-9s|%-6d|%s\n", TCP, port, service);
+                System.out.printf(OUTPUT_FORMAT, TCP, port, service);
             }
 
-            try {
-                DatagramSocket datagramSocket = new DatagramSocket(port);
-                datagramSocket.close();
-            } catch (IOException e) {
-                String service = portMap.get(port);
-                if(service == null) {
-                    service = "---";
+            if (isUdpBusy(port)) {
+                String service = PORT_MAP.get(port);
+                if (service == null) {
+                    service = EMPTY_DESCRIPTION;
                 }
-                System.out.printf("%-9s|%-6d|%s\n", UDP, port, service);
+                System.out.printf(OUTPUT_FORMAT, UDP, port, service);
             }
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        scanner();
+    public static boolean isTcpBusy(Integer port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.close();
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
+    }
+
+    public static boolean isUdpBusy(Integer port) {
+        try {
+            DatagramSocket datagramSocket = new DatagramSocket(port);
+            datagramSocket.close();
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
     }
 }
